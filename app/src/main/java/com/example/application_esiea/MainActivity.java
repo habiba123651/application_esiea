96ae136776280ca34717b22417e8d19204878500
habiba123651 <habiba.alaoui55@gmail.com>
2020-05-21 20:21:02 +0200

    Ajout de la liste

--- app/src/main/java/com/example/application_esiea/MainActivity.java
+++ app/src/main/java/com/example/application_esiea/MainActivity.java
@@ -10,16 +10,22 @@ import android.widget.TextView;
 import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 ;import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
 
-private TextView mGreetingText;
-private EditText mNameInput;
-private Button mPlayButton;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ListAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }
+        // define an adapter
+        mAdapter = new ListAdapter(input);
+        recyclerView.setAdapter(mAdapter);
+    }
+
+    ItemTouchHelper.SimpleCallback simpleItemTouchCallback =
+            new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                @Override
+                public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder
+                        target) {
+                    return false;
+                }
+                @Override
+                public void onSwiped(RecyclerView.ViewHolder viewHolder, int swipeDir) {
+                    ArrayDeque input;
+                    input.remove(viewHolder.getAdapterPosition());
+                    ListAdapter.notifyItemRemoved(viewHolder.getAdapterPosition());
+                }
+            };
+    ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachTorecyclerView(recyclerView);
+
+
+
+
+    private void setSupportActionBar(Toolbar toolbar) {
+        //
+    }
+}
 
         //Je rajoute du code
         showList();
@@ -44,29 +89,30 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void setSupportActionBar(Toolbar toolbar) {
+        //
+    }
+
     private void showList() {
         // Afficher la liste
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
 
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings){
+        return true;
+
+        }
         return super.onOptionsItemSelected(item);
-    }
-}
+        }
+        }
