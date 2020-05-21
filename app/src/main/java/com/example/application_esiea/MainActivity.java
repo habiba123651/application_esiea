1315a35b2d2888a32fb31fa1727618c3d8c80e02
habiba123651 <habiba.alaoui55@gmail.com>
2020-05-21 15:47:58 +0200

    Ajout de code

--- app/src/main/java/com/example/application_esiea/MainActivity.java
+++ app/src/main/java/com/example/application_esiea/MainActivity.java
@@ -28,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Je rajoute du code
+
+
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
